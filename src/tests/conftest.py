bde7036aadcb55041fbb78715292bd6f1906ba1c
klotzgal <ggg44466635@gmail.com>
2024-10-13 13:46:39 +0300

    fix: format

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -6,30 +6,26 @@ from python import Graph
 @pytest.fixture(scope="session")
 def normal_graph():
     g: Graph = Graph()
-    g.load_graph_from_file(
-        "examples/graph.txt")
+    g.load_graph_from_file("examples/graph.txt")
     yield g
 
 
 @pytest.fixture(scope="session")
 def non_full_graph():
     g: Graph = Graph()
-    g.load_graph_from_file(
-        "examples/non_full.txt")
+    g.load_graph_from_file("examples/non_full.txt")
     yield g
 
 
 @pytest.fixture(scope="session")
 def loop_graph():
     g: Graph = Graph()
-    g.load_graph_from_file(
-        "examples/loop.txt")
+    g.load_graph_from_file("examples/loop.txt")
     yield g
 
 
 @pytest.fixture(scope="session")
 def or_graph():
     g: Graph = Graph()
-    g.load_graph_from_file(
-        "examples/or_graph.txt")
+    g.load_graph_from_file("examples/or_graph.txt")
     yield g
--- src/tests/test_dfs.py
+++ src/tests/test_dfs.py
@@ -1,4 +1,5 @@
 import pytest
+
 from python import Graph, GraphAlgorithms
 
 
