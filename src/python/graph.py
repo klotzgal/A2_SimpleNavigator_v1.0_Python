c6077966f84bbeb0ffee887dbc4f51a6a8f07fef
Celestine Valrie <celestiv@student.21-school.ru>
2024-12-12 15:26:51 +0300

    добавлен метод export_graph_to_dot

--- src/python/graph.py
+++ src/python/graph.py
@@ -1,6 +1,4 @@
-import logging
-
-logger = logging.getLogger(__name__)
+import os
 
 
 class Graph:
@@ -18,14 +16,27 @@ class Graph:
 
     def load_graph_from_file(self, filename: str) -> None:
         self._data = []
+        if not os.path.exists(filename):
+            raise FileNotFoundError
         with open(filename, "r") as f:
             matrix_size = int(f.readline().strip())
-            logger.info(f"matrix size: {matrix_size}")
             for _ in range(matrix_size):
                 self._data.append(list(map(int, f.readline().strip().split())))
 
     def export_graph_to_dot(self, filename: str) -> None:
-        pass
+        directory = os.path.dirname(filename)
+        if directory and not os.path.exists(directory):
+            raise FileNotFoundError(f"{directory} не существует! Создайте")
+
+        graphname = os.path.basename(filename).removesuffix(".txt")
+        print(f"Exporting graph to {filename} with graphname {graphname}")
+        with open(filename, 'w') as f:
+            f.write(f"graph {graphname} \n")
+            for i in range(len(self)):
+                for j in range(len(self[i])):
+                    if (i != j) and self[i][j] != 0:
+                        f.write(f"    {i} -- {j};\n")
+            f.write("}\n")
 
     def print_graph(self) -> None:
         for _ in range(len(self._data)):
